6beb26370a0ff2ddcd5fcd96e8ba91594f798e21
jzhu <jzhu@jzhus-MacBook-Pro.local>
2023-07-27 11:18:51 -0700

    customize CategoryMultiChoiceField raw_value function: 'false' (a string) is converted to False by json.loads, convert the value Fasle back to 'false' a string

--- fields/CategoryMultiChoiceField.py
+++ fields/CategoryMultiChoiceField.py
@@ -1,6 +1,21 @@
 from appyter.profiles.default.fields.MultiChoiceField import MultiChoiceField
+from appyter.ext.json import try_json_loads
 
 class CategoryMultiChoiceField(MultiChoiceField):
+    @property
+    def raw_value(self):
+        value = try_json_loads(self.args['value'])
+        if value == False:
+            value = 'false'
+        if not value:
+            return []
+        elif type(value) == list:
+            return value
+        elif type(value) == str:
+            return [value]
+        else:
+            return [self.args['value']]
+
     def constraint(self):
         return True
         #return self.raw_value is not None and all(v in self.choices for v in self.raw_value)
